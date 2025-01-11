38cc34521b2eceb900df4db05926de993ba64113
Sergey Peresypkin <93827706+Segaoneboy@users.noreply.github.com>
2025-01-12 00:47:25 +0500

    third commit

--- src/components/ProjectBlock.jsx
+++ src/components/ProjectBlock.jsx
@@ -4,10 +4,10 @@ const ProjectBlock = ({image,text, href}) => {
     return (
         <div >
             <div className="flex justify-center">
-                <img src={image} alt="project preview" className="border-4 rounded-lg border-white w-1/2"/>
+                <img src={image} alt="project preview" className="border-2 rounded-lg border-white w-1/2"/>
             </div>
             <div className="flex justify-center my-4">
-                <a className="text-center text-2xl" href={href}>
+                <a className="text-center text-2xl underline"  href={href}>
                     {text}
                 </a>
             </div>

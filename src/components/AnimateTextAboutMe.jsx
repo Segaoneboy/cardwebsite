96fd5bc7d4d40b17839d1e6e5d5492beb4c8b30b
Sergey Peresypkin <93827706+Segaoneboy@users.noreply.github.com>
2025-01-20 21:27:36 +0500

    0.1.1

--- src/components/AnimateTextAboutMe.jsx
+++ src/components/AnimateTextAboutMe.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 const AnimatedTextAboutMe = () => {
     const text = `
@@ -10,28 +11,40 @@ const AnimatedTextAboutMe = () => {
     Основатель и лидер команды Sycroll Arts.<br/>
   `;
 
+    // Настройка useInView для отслеживания области видимости
+    const [ref, inView] = useInView({
+        triggerOnce: true, // Анимация сработает только один раз
+        threshold: 0.1, // Компонент должен быть виден на 10% для активации
+    });
+
+    const containerVariants = {
+        hidden: { opacity: 1 },
+        visible: {
+            opacity: 1,
+            transition: {
+                staggerChildren: 0.05, // Задержка между символами
+            },
+        },
+    };
+
+    const charVariants = {
+        hidden: { opacity: 0, y: 10 },
+        visible: { opacity: 1, y: 0 },
+    };
+
     return (
         <motion.div
+            ref={ref} // Привязка элемента к useInView
             className="lg:ml-40 px-12 md:px-28 my-4 font-bold md:text-2xl text-xl text-center content-center"
             initial="hidden"
-            animate="visible"
-            variants={{
-                hidden: { opacity: 1 },
-                visible: {
-                    opacity: 1,
-                    transition: {
-                        staggerChildren: 0.1,
-                    },
-                },
-            }}
+            animate={inView ? "visible" : "hidden"} // Анимация запускается при попадании в область видимости
+            variants={containerVariants}
         >
             {text.split(/(<br\/>)/).map((char, index) => (
                 <motion.span
                     key={index}
-                    variants={{
-                        hidden: { opacity: 0 },
-                        visible: { opacity: 1 },
-                    }}
+                    variants={charVariants}
+                    style={{ display: "inline-block" }}
                 >
                     {char === "<br/>" ? <br /> : char}
                 </motion.span>
